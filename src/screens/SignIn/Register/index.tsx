64b1d6a24131a33af1e93a7d2a7e749069671154
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-10-16 16:55:04 -0300

    feat/signInIntegration: sign in screen integrated

--- src/screens/SignIn/Register/index.tsx
+++ src/screens/SignIn/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { FormularyModel } from './interfaces';
