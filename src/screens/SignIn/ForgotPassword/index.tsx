04a427b17c9862a534b4ea94bd6bd5f846134833
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-10-18 16:31:51 -0300

    feat/forgot-pass-integration: screen integrated to back

--- src/screens/SignIn/ForgotPassword/index.tsx
+++ src/screens/SignIn/ForgotPassword/index.tsx
@@ -1,11 +1,15 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { useAxios } from '@/utils/useAxios';
 
 import Main from './main';
 
 const ForgotPassword: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [axiosPost] = useAxios('post');
+  const [userData, setUserData] = useState();
 
   const { goBack } = useNavigation();
 
@@ -18,7 +22,17 @@ const ForgotPassword: React.FC = () => {
   }, []);
 
   const redefinePassword = useCallback(async () => {
-    //TODO
+    console.log(email);
+    console.log(password); 
+
+    await axios.post('http://192.168.15.35:8080/user/forgot-password', 
+      {email: email}).then((data) => {setUserData(data.data.data.id)});
+    
+    console.log(userData);
+
+    await axios.patch(`http://192.168.15.35:8080/user/${userData}`, {
+      password: password,
+    });
   }, []);
 
   const handleBackToSignIn = useCallback(() => {
